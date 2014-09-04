d590252bcefa2a717737b919f19ac8c061bd172b
Merkur <merkur@live.se>
2014-09-04 19:57:08 +0200

    Fresh start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moviecheck::Application.config.secret_key_base = '2c0ee9eb0886e300655de79e526a3352ef0736c3bbbc42d217a62fe9a56e7fdbe61553ca8337f5e3ac5015474a4e9011430a74cef66c67329a4fd550e82760c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Moviecheck::Application.config.session_store :cookie_store, key: '_moviecheck_session'
