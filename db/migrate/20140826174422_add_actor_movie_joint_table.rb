9c5fe1217593014716a6d9acbbe570e77ae01acf
Merkur <merkur@live.se>
2014-09-04 19:53:54 +0200

    Scratch

--- db/migrate/20140826174422_add_actor_movie_joint_table.rb
+++
@@ -1,8 +0,0 @@
-class AddActorMovieJointTable < ActiveRecord::Migration
-  def change
-    create_table :actor_movies, :id => false do |t|
-        t.references :actor
-        t.references :movie
-    end
-  end
-end
--- db/migrate/20140904165555_add_genre_movie_joint_table.rb
+++
@@ -1,8 +0,0 @@
-class AddGenreMovieJointTable < ActiveRecord::Migration
-  def change
-    create_table :genre_movies, :id => false do |t|
-        t.references :genre
-        t.references :movie
-    end
-  end
-end
